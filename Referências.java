dfbc6e8c4388a3224cf70ad9abc9c4af508a3146
fabriciocond1 <114630404+fabriciocond1@users.noreply.github.com>
2022-10-02 20:46:08 -0300

    Create Referências.java

---
+++ Referências.java
@@ -0,0 +1,8 @@
+\\ Referências para desenvolvimento.
+
+https://www.blogson.com.br/como-usar-o-timer-do-java-de-forma-simples/
+http://www.softblue.com.br/blog/java-strings-nao-use-operador-de-igualdade/
+https://www.baeldung.com/java-random-string
+https://docs.oracle.com/javase/6/docs/api/java/lang/String.html#compareTo%28java.lang.String%29
+https://commons.apache.org/proper/commons-lang/javadocs/api-3.9/org/apache/commons/lang3/RandomStringUtils.html
+https://www.youtube.com/watch?v=jsqztViUJlE&t=146s
